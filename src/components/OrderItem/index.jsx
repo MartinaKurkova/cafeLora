53172a8479306d0deef6adacf078276e2b57dda4
Martina Kurkova <martinanoha@seznam.cz>
2025-07-01 19:07:45 +0200

    order_details

---
+++ src/components/OrderItem/index.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+export const OrderItem = ( {name, image} ) => {
+    return (
+        <div className="order-item">
+              <img
+                src={`http://localhost:4000${image}`}
+                alt={name}
+                className="order-item__image"
+              />
+              <div className="order-item__name">
+                {name}
+              </div>
+            </div>
+    );
+};
--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -1,50 +1,28 @@
 import { render } from '@czechitas/render';
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { Order } from "../components/Order";
 import '../global.css';
 import './index.css';
 import './order.css';
+import { OrderItem } from '../components/OrderItem';
 
-document.querySelector('#root').innerHTML = render(
+const loadOrder = async () => {
+  const response = await fetch("http://localhost:4000/api/drinks?filter=ordered:eq:true&select=id,name,image");
+  const json = await response.json();
+  const orderedItems = json.data;
+
+  document.querySelector('#root').innerHTML = render(
   <div className="page">
     <div className="page">
       <Header showMenu={false} />
-
-      <main className="order">
-        <div className="container order__content">
-          <h1>Vaše objedávnka</h1>
-          <p className="empty-order">Zatím nemáte nic objednáno</p>
-          <div className="order__items">
-            <div className="order-item">
-              <img
-                src="/cups/espresso.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Espresso
-              </div>
-            </div>
-
-            <div className="order-item">
-              <img
-                src="/cups/doppio.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Doppio
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-
-      <footer>
-        <div className="container">
-          <div className="footer__content">
-            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-          </div>
-        </div>
-      </footer>
+      <Order items={orderedItems} />
+      <Footer />
     </div>
   </div>
 );
+};
+
+loadOrder();
+
+
